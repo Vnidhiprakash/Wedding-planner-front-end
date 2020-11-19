0e696e173ccad0cf325fdef2f4c434c92534e0d8
Vnidhiprakash <74674538+Vnidhiprakash@users.noreply.github.com>
2020-11-19 16:09:57 +0530

    Add files via upload

---
+++ front_page.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="try_s.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+  <header>
+    <nav>
+       <label class="logo">Wedding <span> planner<span></label>
+       <ul>
+         <li><a href="#">Home</a></li>
+         <li><a href="#">Services</a></li>
+         <li><a href="#">Gallery</a></li>
+         <li><a href="#">About us</a></li>
+         <li><a href="#">Contact us</a></li>
+       </ul>
+       <label id="icon" >
+         <i class="fas fa-bars" style="font-size: 27px;"></i>
+       </label>
+    </nav>
+</header>
+
+<section>
+<div class="container">
+   
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="img1.jpg" alt="Los Angeles" style="width:1400px; height:500px">
+        
+      </div>
+
+      <div class="item">
+        <img src="img3.jpg" alt="Chicago" style="width:1400px; height:500px ">
+      </div>
+    
+      <div class="item">
+        <img src="img2.jpg" alt="New york" style="width:1400px; height:500px">
+      </div>
+    </div>
+
+    
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</section>
+<br>
+<br>
+
+
+
+<h2 class="title">Plan your dream wedding</h2>
+<div class="content clearfix">
+        <div class="main-content">
+            
+
+            <div class="div post">
+                <img src="img4.jpg" alt="" class="post-image">
+                <div class="post-preview">
+                    <h2><a href="#" style="color: brown;"></a></h2>
+                    <p class="preview-text">
+                    Marriage is the eternal bondage of two opposite gender. This event comes at a time of every human’s life. 
+                    The most adorable dream that every girl and boy would posses is the wedding .THeir wouldn't be a single soul who
+                    would not want their wedding to be as a fairy tale.
+                    wedding involves lots of activity that involve hard work, creativity, and execution.
+                    nd we are here to full fill all ur requirements.
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="side-bar">
+            <ul>
+              <li>Ground Availability(100 to 2000 guests)</li>
+              <li>Hall Availability(100 to 1000 guests)</li>
+              <li>DJ, Light, Sound</li>
+              <li>Photoshoot, Video shoot</li>
+              <li>Invitations card</li>
+              <li>Floral Decorations</li>
+              <li>Theme Decoration</li>
+            </ul>
+        </div>
+    </div>
+
+    <br>
+    <h2 class="gallery-title">Take a look at our featured weddings</h2>
+    <br>
+
+<section class="cards-section1">
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="haldi.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>Haldi Ceremony</h1> 
+      <p>Enjoy Your Haldi</p> 
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="sangeet.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>Sangeet Ceremony</h1> 
+      <p>Enjoy Your sangeet ceremony</p> 
+      
+    </div>
+  </div>
+</div>
+
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="beachdest.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>Beach destination wedding</h1> 
+      <p>Enjoy Your wedding</p> 
+      
+    </div>
+  </div>
+</div>
+</section>
+
+<section class="card-section2">
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="wedding.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>Wedding ceremony</h1> 
+      <p>Enjoy your wedding</p> 
+    </div>
+  </div>
+</div>
+
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="mehendi.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>Mehendi ceremony</h1> 
+      <p>Enjoy your ceremony</p> 
+      
+    </div>
+  </div>
+</div>
+
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="engagement.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>Engagement </h1> 
+      <p>Enjoy your dayr</p> 
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+</section>
+<section>
+<h2 class="about-title">About Us</h2>
+<br>
+<br>
+<div class="left-bar">
+            
+                <div class="about-post-preview">
+                    <b><p class="text">We are the one who bring your wedding dream true</p></b>
+                    <p class="preview-text">
+                        If you aure looking for the wedding planner who will consider all your thoughts and 
+                        accordingly will plan your special day, then your on write page.
+                        We had organized more than 100 weddings succesfully.
+                        Our moto is to make you happy by fullfilling your wedding day dreams.
+                        We provide full featured service from pre-wedding photoshoot, videos, catering etc.
+
+                    </p>
+                </div>
+           
+    </div>
+
+        <div class="right-bar">
+        <img src="about.jpg" alt="" class="post-image" height="250px" width="400px" />
+        </div>
+    </section>   
+
+    <footer class="site-footer" style="margin-top: 240px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h4>Contact us</h4>
+            <form>
+              <input type="email" placeholder="email" requried>
+              <br>
+              <br>
+              <textarea placeholder="message" name="w3review" rows="4" cols="50"></textarea>
+              <br>
+              <button type="submit">Send</button>
+
+            </form>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h4>Our pages</h4>
+            <ul class="footer-links">
+              <li><a href=""><span class="fab fa-facebook-f"></span></a></li>
+              <li><a href=""><span class="fab fa-twitter"></a></li>
+              <li><a href=""><span class="fab fa-instagram"></a></li>
+              <li><a href=""><span class="fab fa-youtube"></a></li>
+              <li><a href=""><span class="fab fa-facebook-f"></a></li>
+              
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h4>Address</h4>
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">Andher, Saiwadi</span>
+            </div>
+            <br>
+            <div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">+91-6785578664</span>
+            </div>
+            <br>
+            <div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text">weddingplanner1@gmail.com</span>
+            </div>
+          </div>
+        </div>
+        <hr>
+      </div>
+      
+</footer>
+
+
+</body>
+</html>
